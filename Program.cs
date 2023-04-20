56df59f93527201806d7df25e12d51065ad60788
jberes <jasonberes@gmail.com>
2023-04-19 21:01:01 -0400

    Updated URIs

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddSwaggerGen();
 // Azure KeyVault
 var keyVaultEndpoint = new Uri(builder.Configuration["VaultKey"]);
 var secretClient = new SecretClient(keyVaultEndpoint, new DefaultAzureCredential());
-KeyVaultSecret kvs = secretClient.GetSecret("ProjectTrackerSecret1");
+KeyVaultSecret kvs = secretClient.GetSecret("ProjectTrackerSecret5");
 builder.Services.AddDbContext<ProjectTrackerContext>(o => o.UseSqlServer(kvs.Value));
 
 // Standard connection string
